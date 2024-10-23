8b05db62e7f5daa56a98c765425571da92b919a6
cungtronghau <cungtronghau@gmail.com>
2024-10-23 17:37:46 +0700

    edit

--- dagster/dagster_jupyter/etl/setup.py
+++ dagster/dagster_jupyter/etl/setup.py
@@ -4,8 +4,12 @@ setup(
     name="etl",
     packages=find_packages(exclude=["etl_tests"]),
     install_requires=[
-        "dagster",
+        "dagster==1.8.10",
         "dagster-cloud"
     ],
-    extras_require={"dev": ["dagit", "pytest"]},
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
+
+# pip install dagster dagster-webserver dagster-cloud dagit
+
+# pip uninstall -y dagster dagster-webserver dagster-cloud dagit
